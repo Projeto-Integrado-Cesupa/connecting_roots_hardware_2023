4fdc82e5dfa24bf2371d40846ae5d9aa55b21870
jhefdantas <jeffersonfdantas95@gmail.com>
2023-05-03 14:43:56 -0300

    feat: adição front-end do projeto

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,175 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import "react-pro-sidebar/dist/css/styles.css";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import AssistantOutlinedIcon from '@mui/icons-material/AssistantOutlined';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h4"  color={colors.grey[100]}>
+                  ConnectingRoots
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`/assets/elonmusk.jpeg`}
+                  style={{ cursor: "pointer", borderRadius: "50%", objectFit: "cover" }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Elon Musk
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  CEO da Tesla
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Home"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Meus Projetos"
+              to="/"
+              icon={<BusinessCenterOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Chat"
+              to="/"
+              icon={<ChatOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Organizações"
+              to="/"
+              icon={<BusinessOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SubMenu icon={<SettingsOutlinedIcon />} title="Configurações">
+              <MenuItem icon={<PeopleOutlinedIcon />}>Perfil público<Link to="/" /></MenuItem>
+              <MenuItem icon={<AssistantOutlinedIcon />}>Acessibilidade</MenuItem>
+              <MenuItem icon={<AddCircleOutlineOutlinedIcon />}>Aparência</MenuItem>
+              <MenuItem icon={<NotificationsActiveOutlinedIcon />}>Notificação</MenuItem>
+              <MenuItem icon={<SmsOutlinedIcon />}>Conversas</MenuItem>
+              <MenuItem icon={<BusinessOutlinedIcon />}>Organizações</MenuItem>
+              <MenuItem icon={<BusinessCenterOutlinedIcon />}>Projetos</MenuItem>
+              <MenuItem icon={<AccountCircleOutlinedIcon />}>Conta</MenuItem>
+              <MenuItem icon={<PaidOutlinedIcon />}>Formas de pagamento</MenuItem>
+            </SubMenu>
+            <Item 
+              title="Sair"
+              to="/"
+              icon={<LogoutOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+
+export default Sidebar;
